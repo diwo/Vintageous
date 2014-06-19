78f0cf203fbd5192bf81a401ea072385622535f7
Dickson Wong <dickson.wong@gmail.com>
2014-06-19 01:09:33 -0700

    Renamed ViewTest.assertEqualRegions to assert_equal_regions

--- tests/commands/test__vi_f.py
+++ tests/commands/test__vi_f.py
@@ -47,7 +47,7 @@ class Test_vi_f(ViewTest):
 			self.add_sel(self.R(*data.startRegion))
 			self.view.run_command('_vi_find_in_line',
 				{'mode': data.mode, 'count': 1, 'char': data.findChar, 'inclusive': True})
-			self.assertEqualRegions(self.R(*data.expectedRegion), self.first_sel(),
+			self.assert_equal_regions(self.R(*data.expectedRegion), self.first_sel(),
 				"Failed on index {} {} : Text:\"{}\" Region:{} Find:'{}'"
 					.format(i, data.msg, data.text, data.startRegion, data.findChar))
 
