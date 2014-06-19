f7760e44ad352217a6774017a4f532bfa89e0441
Dickson Wong <dickson.wong@gmail.com>
2014-06-18 18:55:34 -0700

    Updated tests for f and t to allow testing multiple modes

--- tests/commands/test__vi_f.py
+++ tests/commands/test__vi_f.py
@@ -3,51 +3,51 @@ from collections import namedtuple
 from Vintageous.tests import ViewTest
 from Vintageous.vi.utils import modes
 
-test_data = namedtuple('test_data', 'text startRegion findChar expectedRegion msg')
+test_data = namedtuple('test_data', 'text startRegion findChar mode expectedRegion msg')
 
-TEST_MULTI_CHAR_CASES = (
-	test_data('0ab3x5', (1, 3), 'x', (1, 5), 'Forward'),
-	test_data('0a23x5', (1, 5), 'x', (1, 5), 'Forward find b'),
-	test_data('0b2xa5', (5, 1), 'x', (5, 3), 'Reverse no crossover'),
-	test_data('0ba3x5', (3, 1), 'x', (2, 5), 'Reverse crossover'),
-	test_data('0b2x45', (4, 1), 'x', (4, 3), 'Reverse find a'),
-	test_data('0x2a45', (4, 1), 'x', (4, 1), 'Reverse find b'),
+VISUAL_MULTI_CHAR_CASES = (
+	test_data('0ab3x5', (1, 3), 'x', modes.VISUAL, (1, 5), 'Forward'),
+	test_data('0a23x5', (1, 5), 'x', modes.VISUAL, (1, 5), 'Forward find b'),
+	test_data('0b2xa5', (5, 1), 'x', modes.VISUAL, (5, 3), 'Reverse no crossover'),
+	test_data('0ba3x5', (3, 1), 'x', modes.VISUAL, (2, 5), 'Reverse crossover'),
+	test_data('0b2x45', (4, 1), 'x', modes.VISUAL, (4, 3), 'Reverse find a'),
+	test_data('0x2a45', (4, 1), 'x', modes.VISUAL, (4, 1), 'Reverse find b'),
 )
 
-TEST_ONE_CHAR_CASES = (
-	test_data('ax', (0, 2), 'x', (0, 2), 'Forward find b'),
-	test_data('bx', (2, 0), 'x', (2, 1), 'Reverse find a'),
-	test_data('fx', (0, 1), 'x', (0, 2), 'Forward find next'),
-	test_data('rx', (1, 0), 'x', (0, 2), 'Reverse find next'),
-	test_data('f',  (0, 1), 'f', (0, 1), 'Forward find self'),
-	test_data('r',  (1, 0), 'r', (1, 0), 'Reverse find self'),
+VISUAL_ONE_CHAR_CASES = (
+	test_data('ax', (0, 2), 'x', modes.VISUAL, (0, 2), 'Forward find b'),
+	test_data('bx', (2, 0), 'x', modes.VISUAL, (2, 1), 'Reverse find a'),
+	test_data('fx', (0, 1), 'x', modes.VISUAL, (0, 2), 'Forward find next'),
+	test_data('rx', (1, 0), 'x', modes.VISUAL, (0, 2), 'Reverse find next'),
+	test_data('f',  (0, 1), 'f', modes.VISUAL, (0, 1), 'Forward find self'),
+	test_data('r',  (1, 0), 'r', modes.VISUAL, (1, 0), 'Reverse find self'),
 )
 
-TEST_MULTI_MATCHES_CASES = (
-	test_data('0abxx5', (1, 3), 'x', (1, 4), 'Forward find first'),
-	test_data('0axx45', (1, 3), 'x', (1, 4), 'Forward find b'),
-	test_data('0bxx45', (3, 1), 'x', (3, 2), 'Reverse find a'),
-	test_data('0bxx45', (4, 1), 'x', (4, 2), 'Reverse find a'),
-	test_data('0xax45', (3, 1), 'x', (2, 4), 'Reverse find b'),
+VISUAL_MULTI_MATCHES_CASES = (
+	test_data('0abxx5', (1, 3), 'x', modes.VISUAL, (1, 4), 'Forward find first'),
+	test_data('0axx45', (1, 3), 'x', modes.VISUAL, (1, 4), 'Forward find b'),
+	test_data('0bxx45', (3, 1), 'x', modes.VISUAL, (3, 2), 'Reverse find a'),
+	test_data('0bxx45', (4, 1), 'x', modes.VISUAL, (4, 2), 'Reverse find a'),
+	test_data('0xax45', (3, 1), 'x', modes.VISUAL, (2, 4), 'Reverse find b'),
 )
 
-class Test_f_visual_mode(ViewTest):
+class Test_vi_f(ViewTest):
 	def runTests(self, data):
 		for (i, data) in enumerate(data):
 			self.write(data.text)
 			self.clear_sel()
 			self.add_sel(self.R(*data.startRegion))
 			self.view.run_command('_vi_find_in_line',
-				{'mode': modes.VISUAL, 'count': 1, 'char': data.findChar, 'inclusive': True})
+				{'mode': data.mode, 'count': 1, 'char': data.findChar, 'inclusive': True})
 			self.assertEqualRegions(self.R(*data.expectedRegion), self.first_sel(),
 				"Failed on index {} {} : Text:\"{}\" Region:{} Find:'{}'"
 					.format(i, data.msg, data.text, data.startRegion, data.findChar))
 
-	def testMultipleCharacterCases(self):
-		self.runTests(TEST_MULTI_CHAR_CASES)
+	def testVisualMultipleCharacterCases(self):
+		self.runTests(VISUAL_MULTI_CHAR_CASES)
 
-	def testSingleCharacterCases(self):
-		self.runTests(TEST_ONE_CHAR_CASES)
+	def testVisualSingleCharacterCases(self):
+		self.runTests(VISUAL_ONE_CHAR_CASES)
 
-	def testMultipleMatchesCase(self):
-		self.runTests(TEST_MULTI_MATCHES_CASES)
+	def testVisualMultipleMatchesCase(self):
+		self.runTests(VISUAL_MULTI_MATCHES_CASES)
--- tests/commands/test__vi_t.py
+++ tests/commands/test__vi_t.py
@@ -3,54 +3,54 @@ from collections import namedtuple
 from Vintageous.tests import ViewTest
 from Vintageous.vi.utils import modes
 
-test_data = namedtuple('test_data', 'text startRegion findChar expectedRegion msg')
+test_data = namedtuple('test_data', 'text startRegion findChar mode expectedRegion msg')
 
-TEST_MULTI_CHAR_CASES = (
-	test_data('0ab3x5', (1, 3), 'x', (1, 4), 'Forward'),
-	test_data('0a23x5', (1, 5), 'x', (1, 5), 'Forward find b'),
-	test_data('0b2xa5', (5, 1), 'x', (5, 2), 'Reverse no crossover'),
-	test_data('0ba3x5', (3, 1), 'x', (2, 4), 'Reverse crossover'),
-	test_data('0b2x45', (4, 1), 'x', (4, 2), 'Reverse find a'),
-	test_data('0x2a45', (4, 1), 'x', (4, 1), 'Reverse find b'),
-	test_data('0a2bx5', (1, 4), 'x', (1, 4), 'Forward find b+1'),
-	test_data('0bx3a5', (5, 1), 'x', (5, 1), 'Reverse find b+1'),
-	test_data('0b2ax5', (4, 1), 'x', (4, 3), 'Reverse find a+1'),
+VISUAL_MULTI_CHAR_CASES = (
+	test_data('0ab3x5', (1, 3), 'x', modes.VISUAL, (1, 4), 'Forward'),
+	test_data('0a23x5', (1, 5), 'x', modes.VISUAL, (1, 5), 'Forward find b'),
+	test_data('0b2xa5', (5, 1), 'x', modes.VISUAL, (5, 2), 'Reverse no crossover'),
+	test_data('0ba3x5', (3, 1), 'x', modes.VISUAL, (2, 4), 'Reverse crossover'),
+	test_data('0b2x45', (4, 1), 'x', modes.VISUAL, (4, 2), 'Reverse find a'),
+	test_data('0x2a45', (4, 1), 'x', modes.VISUAL, (4, 1), 'Reverse find b'),
+	test_data('0a2bx5', (1, 4), 'x', modes.VISUAL, (1, 4), 'Forward find b+1'),
+	test_data('0bx3a5', (5, 1), 'x', modes.VISUAL, (5, 1), 'Reverse find b+1'),
+	test_data('0b2ax5', (4, 1), 'x', modes.VISUAL, (4, 3), 'Reverse find a+1'),
 )
 
-TEST_ONE_CHAR_CASES = (
-	test_data('ax', (0, 2), 'x', (0, 2), 'Forward find b'),
-	test_data('bx', (2, 0), 'x', (2, 0), 'Reverse find a'),
-	test_data('fx', (0, 1), 'x', (0, 1), 'Forward find next'),
-	test_data('rx', (1, 0), 'x', (1, 0), 'Reverse find next'),
-	test_data('f',  (0, 1), 'f', (0, 1), 'Forward find self'),
-	test_data('r',  (1, 0), 'r', (1, 0), 'Reverse find self'),
+VISUAL_ONE_CHAR_CASES = (
+	test_data('ax', (0, 2), 'x', modes.VISUAL, (0, 2), 'Forward find b'),
+	test_data('bx', (2, 0), 'x', modes.VISUAL, (2, 0), 'Reverse find a'),
+	test_data('fx', (0, 1), 'x', modes.VISUAL, (0, 1), 'Forward find next'),
+	test_data('rx', (1, 0), 'x', modes.VISUAL, (1, 0), 'Reverse find next'),
+	test_data('f',  (0, 1), 'f', modes.VISUAL, (0, 1), 'Forward find self'),
+	test_data('r',  (1, 0), 'r', modes.VISUAL, (1, 0), 'Reverse find self'),
 )
 
-TEST_MULTI_MATCHES_CASES = (
-	test_data('0abxx5', (1, 3), 'x', (1, 3), 'Forward find first'),
-	test_data('0axx45', (1, 3), 'x', (1, 3), 'Forward find b'),
-	test_data('0bxx45', (3, 1), 'x', (3, 1), 'Reverse find a'),
-	test_data('0bxx45', (4, 1), 'x', (4, 1), 'Reverse find a'),
-	test_data('0xax45', (3, 1), 'x', (3, 2), 'Reverse find b'),
+VISUAL_MULTI_MATCHES_CASES = (
+	test_data('0abxx5', (1, 3), 'x', modes.VISUAL, (1, 3), 'Forward find first'),
+	test_data('0axx45', (1, 3), 'x', modes.VISUAL, (1, 3), 'Forward find b'),
+	test_data('0bxx45', (3, 1), 'x', modes.VISUAL, (3, 1), 'Reverse find a'),
+	test_data('0bxx45', (4, 1), 'x', modes.VISUAL, (4, 1), 'Reverse find a'),
+	test_data('0xax45', (3, 1), 'x', modes.VISUAL, (3, 2), 'Reverse find b'),
 )
 
-class Test_t_visual_mode(ViewTest):
+class Test_vi_t(ViewTest):
 	def runTests(self, data):
 		for (i, data) in enumerate(data):
 			self.write(data.text)
 			self.clear_sel()
 			self.add_sel(self.R(*data.startRegion))
 			self.view.run_command('_vi_find_in_line',
-				{'mode': modes.VISUAL, 'count': 1, 'char': data.findChar, 'inclusive': False})
+				{'mode': data.mode, 'count': 1, 'char': data.findChar, 'inclusive': False})
 			self.assertEqualRegions(self.R(*data.expectedRegion), self.first_sel(),
 				"Failed on index {} {} : Text:\"{}\" Region:{} Find:'{}'"
 					.format(i, data.msg, data.text, data.startRegion, data.findChar))
 
-	def testMultipleCharacterCases(self):
-		self.runTests(TEST_MULTI_CHAR_CASES)
+	def testVisualMultipleCharacterCases(self):
+		self.runTests(VISUAL_MULTI_CHAR_CASES)
 
-	def testSingleCharacterCases(self):
-		self.runTests(TEST_ONE_CHAR_CASES)
+	def testVisualSingleCharacterCases(self):
+		self.runTests(VISUAL_ONE_CHAR_CASES)
 
-	def testMultipleMatchesCase(self):
-		self.runTests(TEST_MULTI_MATCHES_CASES)
+	def testVisualMultipleMatchesCase(self):
+		self.runTests(VISUAL_MULTI_MATCHES_CASES)
